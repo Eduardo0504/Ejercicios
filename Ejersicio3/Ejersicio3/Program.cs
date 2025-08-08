2b26e92728bdc398745beaff40be9a00b21e43b9
Eduardo0504 <maujohernav50@gmail.com>
2025-08-08 09:14:51 -0600

    Ejercicio 3

---
+++ Ejersicio3/Ejersicio3/Program.cs
@@ -0,0 +1,12 @@
+﻿int numeros = 0;
+int cantidad = 0;
+int contadorSuma = 0;
+Console.WriteLine("Escriba la cantidad de numeros a sumar? ");
+cantidad = int.Parse(Console.ReadLine());
+for (int contador = 1; contador <= cantidad; contador ++)
+{
+	Console.WriteLine($"Escriba el numero {contador}: ");
+	numeros = int.Parse( Console.ReadLine());
+	contadorSuma = contadorSuma + numeros;
+}
+Console.WriteLine($"el resultado de la suma es:" + contadorSuma );
