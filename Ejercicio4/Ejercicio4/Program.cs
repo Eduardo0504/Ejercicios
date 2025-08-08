af144fdbcc3be4db2df7dcb7a3369c4e323ddd89
Eduardo0504 <maujohernav50@gmail.com>
2025-08-08 09:56:32 -0600

    Ejercicio 4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,13 @@
+﻿int retiro = 0;
+int saldo = 100;
+Console.WriteLine("cuanto dinero desea retirar? ");
+retiro= int.Parse(Console.ReadLine());
+
+if ( saldo < retiro )
+{
+	Console.WriteLine("su saldo es insuficiente");
+}
+else
+{
+	Console.WriteLine("su retiro fue exitoso");
+}
